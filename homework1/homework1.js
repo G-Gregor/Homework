a15bd9b21022d93520f1d704f7e5b1a38a65beff
Gregor <Gregor199223@gmail.com>
2021-11-26 20:02:19 +0200

    first task

---
+++ homework1/homework1.js
@@ -0,0 +1,73 @@
+/* Task 2 */
+
+console.log('Пермяков');
+
+/* Task 3 */
+
+// a, b
+let seconds = 60;
+let minutes = 1;
+
+// c
+alert(seconds);
+alert(minutes);
+
+// d
+ minutes = seconds;
+// e
+alert(minutes);
+
+
+/* Task 4 */
+
+const player = {
+    login: 'Snake',
+    age: 29,
+    donate: false,
+    email: null,
+    gender: undefined
+}
+
+/* Task 5 */
+
+let isAdault = confirm('Are you over 18?');
+alert(isAdault);
+
+/* Task 6 */
+
+const myName = 'Григорій';
+const surName = 'Пермяков';
+const studyGroup = '654.JS Core';
+const yearBirth = 1992;
+let married = true;
+
+console.log(typeof yearBirth);
+console.log(typeof married);
+console.log(typeof myName);
+console.log(typeof surName);
+console.log(typeof studyGroup);
+
+let mail = null;
+let age = undefined;
+
+console.log(typeof mail);
+console.log(typeof age);
+
+/* Task 7 */
+
+let yourLogin = prompt('Your login, please ');
+alert(yourLogin);
+let yourEmail = prompt('Your email, please');
+alert(yourEmail);
+let password = prompt('Your password, please');
+alert(password);
+
+/* Task 8 */
+
+let hour = seconds * 60;
+alert(hour);
+let day = hour * 24;
+alert(day);
+let month = day * 30;
+alert(month);
+
