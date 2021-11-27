6b7d9badcb7ed4af9116d6da7ca238ecdf24c3bb
Gregor <Gregor199223@gmail.com>
2021-11-27 13:20:08 +0200

    correction

--- homework1/homework1.js
+++ homework1/homework1.js
@@ -1,73 +1,86 @@
 /* Task 2 */
 
-console.log('Пермяков');
+    console.log('Пермяков');
 
 /* Task 3 */
 
-// a, b
-let seconds = 60;
-let minutes = 1;
+    // a, b
+        let second = 1;
+        let time = 'секунда';
 
-// c
-alert(seconds);
-alert(minutes);
+    // c
+        alert(second);
+        alert(time);
 
-// d
- minutes = seconds;
-// e
-alert(minutes);
+    // d
+        time = second;
+
+    // e
+        alert(time);
 
 
 /* Task 4 */
 
-const player = {
-    login: 'Snake',
-    age: 29,
-    donate: false,
-    email: null,
-    gender: undefined
-}
+    const player = {
+        login: 'Snake',
+        age: 29,
+        donate: false,
+        email: null,
+        gender: undefined
+    }
+
 
 /* Task 5 */
 
-let isAdault = confirm('Are you over 18?');
-alert(isAdault);
+    let isAdault = confirm('Are you over 18?');
+
+    alert(isAdault);
 
 /* Task 6 */
 
-const myName = 'Григорій';
-const surName = 'Пермяков';
-const studyGroup = '654.JS Core';
-const yearBirth = 1992;
-let married = true;
+    const myName = 'Григорій';
+    const surName = 'Пермяков';
+    const studyGroup = '654.JS Core';
+    const yearBirth = 1992;
+    let married = true;
 
-console.log(typeof yearBirth);
-console.log(typeof married);
-console.log(typeof myName);
-console.log(typeof surName);
-console.log(typeof studyGroup);
+    console.log(typeof yearBirth);
+    console.log(typeof married);
+    console.log(typeof myName);
+    console.log(typeof surName);
+    console.log(typeof studyGroup);
 
-let mail = null;
-let age = undefined;
+    let mail = null;
+    let age ; 
 
-console.log(typeof mail);
-console.log(typeof age);
+    console.log(typeof mail);
+    console.log(typeof age);
 
 /* Task 7 */
 
-let yourLogin = prompt('Your login, please ');
-alert(yourLogin);
-let yourEmail = prompt('Your email, please');
-alert(yourEmail);
-let password = prompt('Your password, please');
-alert(password);
+    let yourLogin = prompt('Your login, please ');
+
+    alert(yourLogin);
+
+    let yourEmail = prompt('Your email, please');
+
+    alert(yourEmail);
+
+    let password = prompt('Your password, please');
+
+    alert(password);
 
 /* Task 8 */
 
-let hour = seconds * 60;
-alert(hour);
-let day = hour * 24;
-alert(day);
-let month = day * 30;
-alert(month);
+    let hour = second * 60 * 60;
+
+    alert(hour);
+
+    let day = hour * 24;
+
+    alert(day);
+
+    let month = day * 30;
+
+    alert(month);
 
